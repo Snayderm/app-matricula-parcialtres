5c753f07af068e14001e1c4ea08f026c5c4c6ef8
Snayderm <sneyder1002m@gmail.com>
2023-11-20 00:39:32 -0500

    router carreras

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CarreraController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/carreras',[CarreraController::class, 'index'])->name('carreras.index');
+    Route::post('/carreras',[CarreraController::class, 'store'])->name('carreras.store');
+    Route::get('/carreras/create',[CarreraController::class, 'create'])->name('carreras.create');
+    Route::delete('/carreras/{carrera}',[CarreraController::class, 'destroy'])->name('carreras.destroy');
+    Route::put('/carreras/{carrera}',[CarreraController::class, 'update'])->name('carreras.update');
+    Route::get('/carreras/{carrera}/edit',[CarreraController::class, 'edit'])->name('carreras.edit');
+
 });
 
 require __DIR__.'/auth.php';
