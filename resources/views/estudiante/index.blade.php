c17b330cfb055af01cd4f071128950c16ed70780
Knight <121831468+knight-gangsta@users.noreply.github.com>
2023-11-21 17:20:04 -0500

    Definitivo Color,Imagen,Inicio

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -11,57 +11,50 @@
     <title>Lista Estudiantes</title>
   </head>
   <body>       
-        <x-app-layout>
-            <x-slot name="header">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    {{ __('Estudiantes') }}
-                 </h2>
-                </x-slot>
-            <div class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="p-6 text-gray-900">   
-    <a href="{{route('estudiantes.create') }}"
-    class="btn btn-success"
-    >Add</a>
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Apellido</th>
-            <th scope="col">Documento</th>
-            <th scope="col">E-mail</th>
-            <th scope="col">Celular</th>
-            <th scope="col">Carrera</th>
-            <th scope="col">Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($estudiantes as $estudiante)
-        <tr>
-            <th scope="row">{{ $estudiante->id }}</th>
-            <td>{{ $estudiante->nombre }}</td>
-            <td>{{ $estudiante->apellido }}</td>
-            <td>{{ $estudiante->documento }}</td>
-            <td>{{ $estudiante->email }}</td>
-            <td>{{ $estudiante->celular }}</td>
-            <td>{{ $estudiante->nombre_carrera }}</td>            
-            <td> 
-              <a href="{{ route('estudiantes.edit', ['estudiante' => $estudiante->id]) }}"
-                class="btn btn-info">
-                Edit</a>
-              <form
-                  action="{{ route('estudiantes.destroy', ['estudiante' => $estudiante->id]) }}"
-                  method='POST' style="display: inline-block">
-                  @method('delete')
-                  @csrf
-                  <input
-                  class="btn btn-danger" type="submit" value="Delete">
-              </form>            
-            </td>                
-        </tr>
-        @endforeach    
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Estudiantes') }}
+             </h2>
+        </x-slot>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">   
+                        <a href="{{ route('estudiantes.create') }}" class="btn btn-success">Add</a>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Apellido</th>
+                                    <th scope="col">Documento</th>
+                                    <th scope="col">E-mail</th>
+                                    <th scope="col">Celular</th>
+                                    <th scope="col">Carrera</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($estudiantes as $estudiante)
+                                <tr>
+                                    <th scope="row">{{ $estudiante->id }}</th>
+                                    <td>{{ $estudiante->nombre }}</td>
+                                    <td>{{ $estudiante->apellido }}</td>
+                                    <td>{{ $estudiante->documento }}</td>
+                                    <td>{{ $estudiante->email }}</td>
+                                    <td>{{ $estudiante->celular }}</td>
+                                    <td>{{ $estudiante->nombre_carrera }}</td>            
+                                    <td> 
+                                        <a href="{{ route('estudiantes.edit', ['estudiante' => $estudiante->id]) }}" class="btn btn-info">Edit</a>
+                                        <form action="{{ route('estudiantes.destroy', ['estudiante' => $estudiante->id]) }}" method='POST' style="display: inline-block">
+                                            @method('delete')
+                                            @csrf
+                                            <button class="btn btn-danger" type="submit">Delete</button>
+                                        </form>            
+                                    </td>                
+                                </tr>
+                                @endforeach    
                             </tbody>
                         </table> 
                     </div>
@@ -71,3 +64,4 @@
     </x-app-layout>     
   </body>
 </html>
+
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-black text-white border-b border-gray-800">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="https://servicios.cecep.edu.co/portal_docente/img/logo_vertical.svg" alt="Logo" class="block h-9 w-auto" />
                     </a>
                 </div>
 
@@ -15,6 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    
+                    <x-nav-link :href="route('estudiantes.index')" :active="request()->routeIs('estudiantes.index')">
+                        {{ __('Estudiantes') }}
+                    </x-nav-link>
+                
+                    <x-nav-link :href="route('carreras.index')" :active="request()->routeIs('carreras.index')">
+                        {{ __('Carreras') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -22,7 +30,7 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-black hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
@@ -54,7 +62,7 @@
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-300 hover:bg-gray-600 focus:outline-none focus:bg-gray-600 focus:text-gray-300 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -73,10 +81,10 @@
         </div>
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-t border-gray-600">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <div class="font-medium text-base text-gray-300">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-sm text-gray-400">{{ Auth::user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
